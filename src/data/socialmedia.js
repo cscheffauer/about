ad1a93516079538707e49940225e051810cb2f23
Christoph Scheffauer <scheffauerc@gmail.com>
2019-12-23 00:50:39 +0100

    created socialmedia data

---
+++ src/data/socialmedia.js
@@ -0,0 +1,10 @@
+const socialmedia = [{
+    "type": "github",
+    "url": "https://github.com/cscheffauer"
+}, {
+    "type": "linkedin",
+    "url": "https://linkedin.com"
+}, {
+    "type": "instagram",
+    "url": "https://instagr.am/christoph.codes"
+}]
