570edcf9d7fa86f0fb7caded73bc4a255ba6fdf2
Christoph Scheffauer <scheffauerc@gmail.com>
2019-12-29 13:13:04 +0100

    styled react slider

---
+++ src/components/Pages/Project.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './Projects.scss';
+
+
+const Project = () => {
+    return <div className="project">Project will displayed be here</div>
+}
+
+export default Project;
--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import Slider from "react-slick";
 
 import './Projects.scss';
 
 
 const Projects = () => {
-    return (
-            <div className="outerBox">
-                    
+
+        var settings = {
+                dots: true
+        };
+        return (
+                <div className="outerBox">
+                        <Slider className="innerBox" {...settings}>
+                                {
+                                        this.state.projects.map((project, i) => {
+                                                return (
+                                                        <Project key={i} name={project.name} demourl={project.demourl} repourl={project.repourl} technologies={project.technologies} />
+                                                );
+                                        })
+                                }
+
+
+
+                                <div>
+                                        <img src="http://placekitten.com/g/400/200" />
+                                </div>
+                                <div>
+                                        <img src="http://placekitten.com/g/400/200" />
+                                </div>
+                                <div>
+                                        <img src="http://placekitten.com/g/400/200" />
+                                </div>
+                                <div>
+                                        <img src="http://placekitten.com/g/400/200" />
+                                </div>
+                        </Slider>
                 </div>
         )
 }
