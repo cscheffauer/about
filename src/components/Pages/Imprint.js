0ca649d604e7ddbc4b3942b13c482c2f03e0e7a2
Christoph Scheffauer <scheffauerc@gmail.com>
2019-12-30 15:01:50 +0100

    added imprint data and visualized it

--- src/components/Pages/Imprint.js
+++ src/components/Pages/Imprint.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+import { imprint } from '../../data/imprint'
+import Scroll from '../Layout/Tools/Scroll'
 
 import './Imprint.scss';
 
 
-const Imprint = () => {
-    return <div className="imprint">Imprint</div>
+class Imprint extends Component {
+    constructor() {
+        super();
+        this.state = {
+            imprint: imprint
+        };
+    }
+    render() {
+        return (
+            <div className="imprintOuterBox" >
+                <Scroll>
+                    {imprint.text}
+                </Scroll>
+            </div>
+        )
+
+    }
 }
 
 export default Imprint;
