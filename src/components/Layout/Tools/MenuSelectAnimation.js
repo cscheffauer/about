de47fe8c0f12dc40c0cd6878eddc15d048bb6ead
Christoph Scheffauer <scheffauerc@gmail.com>
2020-02-05 09:51:50 +0100

    everything is now responsive on mobile ( < 600px)

--- src/components/Layout/Tools/MenuSelectAnimation.js
+++ src/components/Layout/Tools/MenuSelectAnimation.js
@@ -7,13 +7,15 @@ const MenuSelectAnimation = ({ type }) => {
         case 'homeNavIcon':
             return (
                 <svg>
-                    <path className="path" fill="white" stroke="black" strokeWidth="6" d="M -3 135 Q 0 90 45 90 Q 87 87 90 45 Q 90 0 135 -3 " />
+                    <path className="pathDesktop" fill="white" stroke="black" strokeWidth="6" d="M -3 135 Q 0 90 45 90 Q 87 87 90 45 Q 90 0 135 -3 " />
                 </svg>
             );
         case 'NavIcon':
             return (
                 <svg>
-                    <path className="path" fill="white" stroke="black" strokeWidth="6" d="M 0 -3 Q 45 0 47 45 A 43 45 0 1 0 133 45 Q 135 0 180 -3" />
+                    <path className="pathDesktop" fill="white" stroke="black" strokeWidth="6" d="M 0 -3 Q 45 0 47 45 A 43 45 0 1 0 133 45 Q 135 0 180 -3" />
+                    <path className="pathMobile" fill="none" stroke="black" strokeWidth="5" d="M 0 85 Q 50 100 53 45 A 36 40 0 1 1 127 45 Q 130 100 185 90" />
+
                 </svg>
             );
         default: return <Fragment />
