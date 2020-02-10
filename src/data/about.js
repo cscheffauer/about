90466f7637f5be5b649d9f2a711a46695688f537
Christoph Scheffauer <scheffauerc@gmail.com>
2020-02-10 18:28:28 +0100

    ADDED tablet responsiveness at projects, about

--- src/data/about.js
+++ src/data/about.js
@@ -4,9 +4,12 @@ export const about = {
     googleMapsUrl: 'https://www.google.com/maps/search/?api=1&query=Salzburg',
     questions: [{
         question: 'How does your day look like?',
-        answer: "Right now I'm extending my knowledge in React.js, Redux and more advanced web technologies."
+        answer: "Beside my fulltime job as a Media & IT Application Manager I'm extending my knowledge in React.js, Redux and more advanced web technologies 🤓."
     }, {
         question: 'What project are you currently working on and what challenges are you facing?',
-        answer: "Currently I'm working on my portfolio page, where the biggest challenges at the moment are to create a professional and awesome design, which should include some amazing CSS animations. I could spend so many hours with it to create proper animations - kinda perfectionistic I know 😉"
+        answer: "I've just finished my portfolio page, where the biggest challenges have been to create a great design, which includes some awesome CSS animations. I could spend so many hours with it to create proper animations - kinda perfectionistic I know 😉"
+    }, {
+        question: 'What are your future plans?',
+        answer: "The next project I planned to work on is a Newsroom Control System, where I have already created some mockups for it 👨‍🎨. This will be my biggest project for now - it should include some more advanced React techniques, all powered by Redux. "
     }]
 };
