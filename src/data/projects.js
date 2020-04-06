ba67a2661b0e0d8e9944730a7956ad1797a883a2
Christoph Scheffauer <scheffauerc@gmail.com>
2020-04-07 01:25:37 +0200

    Added new evening routine project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,35 @@
 export const projects = [{
+    name: 'Evening Routine',
+    status: {
+        cssclass: 'online',
+        label: 'online'
+    },
+    demourl: {
+        label: 'Live Demo',
+        url: 'https://cscheffauer.github.io/evening_routine/'
+    },
+    repourl: {
+        label: 'Repository',
+        url: 'https://github.com/cscheffauer/evening_routine'
+    },
+    technologies: [
+        {
+            name: 'React'
+        },
+        {
+            name: 'React.lazy'
+        },
+        {
+            name: 'Redux'
+        },
+        {
+            name: 'Redux-Thunk'
+        },
+        {
+            name: 'Material-UI'
+        }
+    ], pictures: []
+}, {
     name: 'Star Wars Figures',
     status: {
         cssclass: 'online',
@@ -64,29 +95,4 @@ export const projects = [{
             src: 'pictures/nrcs_preview2.png'
         }
     ]
-}, {
-    name: 'Lego Set Discovery',
-    status: {
-        cssclass: 'inwork',
-        label: 'coming soon'
-    },
-    demourl: {
-        label: 'coming soon',
-        url: ''
-    },
-    repourl: {
-        label: 'coming soon',
-        url: ''
-    },
-    technologies: [
-        {
-            name: 'React.js'
-        },
-        {
-            name: 'React.lazy'
-        },
-        {
-            name: 'Redux'
-        }
-    ], pictures: []
-}];
+},];
