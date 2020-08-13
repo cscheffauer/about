ce206f3019b2322e10cd35da27018d5601edad52
Christoph Scheffauer <scheffauerc@gmail.com>
2020-08-13 20:27:34 +0200

    edited about questions

--- src/data/about.js
+++ src/data/about.js
@@ -1,15 +1,21 @@
 export const about = {
-    name: 'Christoph Scheffauer',
-    location: 'Salzburg, Austria',
-    googleMapsUrl: 'https://www.google.com/maps/search/?api=1&query=Salzburg',
-    questions: [{
-        question: 'How does your day look like?',
-        answer: "Beside my fulltime job as a Media & IT Application Manager I'm extending my knowledge in React.js, Redux and more advanced web technologies 🤓."
-    }, {
-        question: 'What project are you currently working on and what challenges are you facing?',
-        answer: "I've just finished the first version of my Evening Routine App 🌜, where the biggest challenges have been so far to get to understand Material-UI which I'm using. I've also used Dialogs, nested components and more advanced React techniques, where I've learnt a lot from."
-    }, {
-        question: 'What are your future plans?',
-        answer: "There are so many opportunities to keep up the good work - I would love to contribute on some COVID-19 apps to help building transparency in these hard times we are all facing right now. 😷 What I would also like is to continue working on my Evening routine app, where I need to implement some bug fixes for the frontend first. After that I would like to implement a backend to actually save data and persist evening routines. 🧠"
-    }]
+	name: 'Christoph Scheffauer',
+	location: 'Salzburg, Austria',
+	googleMapsUrl: 'https://www.google.com/maps/search/?api=1&query=Salzburg',
+	questions: [
+		{
+			question: 'How does your day look like?',
+			answer: "Beside my fulltime job as a Media & IT Application Manager I'm extending my knowledge in React.js, Redux and more advanced web technologies 🤓.",
+		},
+		{
+			question: 'What project are you currently working on and what challenges are you facing?',
+			answer:
+				'I have just finished the chat app, where I built a small websocket backend with Node.js. On the frontend side I used only the new React features like hooks, context and more. I learned a lot implementing those new features in a real life project and what advantages and issues are coming with them.',
+		},
+		{
+			question: 'What are your future plans?',
+			answer:
+				'There are so many opportunities to keep up the good work - I would love to step further and even though there are hard times I would like to work fulltime on React frontend development. 😷 Only working really really hard pays off in the end - without fearing obstacles and challenges.',
+		},
+	],
 };
