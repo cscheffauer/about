9082d638127fd7e1fb307b8af20e6d1c9529281e
Christoph Scheffauer <scheffauerc@gmail.com>
2020-08-13 20:22:08 +0200

    Added chat app to the projects and updated modules

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,101 +1,139 @@
-export const projects = [{
-    name: 'Evening Routine',
-    status: {
-        cssclass: 'online',
-        label: 'online'
-    },
-    demourl: {
-        label: 'Live Demo',
-        url: 'https://cscheffauer.github.io/evening_routine/'
-    },
-    repourl: {
-        label: 'Repository',
-        url: 'https://github.com/cscheffauer/evening_routine'
-    },
-    technologies: [
-        {
-            name: 'React'
-        },
-        {
-            name: 'React.lazy'
-        },
-        {
-            name: 'Redux'
-        },
-        {
-            name: 'Redux-Thunk'
-        },
-        {
-            name: 'MaterialUI'
-        }
-    ], pictures: []
-}, {
-    name: 'Pokedex',
-    status: {
-        cssclass: 'online',
-        label: 'online (only Desktop)'
-    },
-    demourl: {
-        label: 'Live Demo',
-        url: 'https://cscheffauer.github.io/pokedex/'
-    },
-    repourl: {
-        label: 'Repository',
-        url: 'https://github.com/cscheffauer/pokedex'
-    },
-    technologies: [
-        {
-            name: 'React'
-        },
-        {
-            name: 'React.lazy'
-        },
-        {
-            name: 'Redux'
-        },
-        {
-            name: 'Redux-Thunk'
-        },
-        {
-            name: 'react-relay (by Facebook)'
-        },
-        {
-            name: 'GraphQL'
-        }
-    ], pictures: []
-}, {
-    name: 'Newsroom Control System',
-    status: {
-        cssclass: 'inwork',
-        label: 'coming soon'
-    },
-    demourl: {
-        label: 'coming soon',
-        url: ''
-    },
-    repourl: {
-        label: 'coming soon',
-        url: ''
-    },
-    technologies: [
-        {
-            name: 'React.js'
-        },
-        {
-            name: 'React.lazy'
-        },
-        {
-            name: 'Redux'
-        }
-    ],
-    pictures: [
-        {
-            name: 'Rundown Mockup',
-            src: 'pictures/nrcs_preview1.png'
-        },
-        {
-            name: 'Story Editor Mockup',
-            src: 'pictures/nrcs_preview2.png'
-        }
-    ]
-},];
+export const projects = [
+	{
+		name: 'Chat App',
+		status: {
+			cssclass: 'online',
+			label: 'online',
+		},
+		repourl: {
+			label: 'Repository',
+			url: 'https://github.com/cscheffauer/chat-app',
+		},
+		technologies: [
+			{
+				name: 'React',
+			},
+			{
+				name: 'React-Linkify',
+			},
+			{
+				name: 'Typescript',
+			},
+			{
+				name: 'Jest & Enzyme',
+			},
+			{
+				name: 'Websocket',
+			},
+			{
+				name: 'Node.js',
+			},
+		],
+		pictures: [],
+	},
+	{
+		name: 'Evening Routine',
+		status: {
+			cssclass: 'online',
+			label: 'online',
+		},
+		demourl: {
+			label: 'Live Demo',
+			url: 'https://cscheffauer.github.io/evening_routine/',
+		},
+		repourl: {
+			label: 'Repository',
+			url: 'https://github.com/cscheffauer/evening_routine',
+		},
+		technologies: [
+			{
+				name: 'React',
+			},
+			{
+				name: 'React.lazy',
+			},
+			{
+				name: 'Redux',
+			},
+			{
+				name: 'Redux-Thunk',
+			},
+			{
+				name: 'MaterialUI',
+			},
+		],
+		pictures: [],
+	},
+	{
+		name: 'Pokedex',
+		status: {
+			cssclass: 'online',
+			label: 'online (only Desktop)',
+		},
+		demourl: {
+			label: 'Live Demo',
+			url: 'https://cscheffauer.github.io/pokedex/',
+		},
+		repourl: {
+			label: 'Repository',
+			url: 'https://github.com/cscheffauer/pokedex',
+		},
+		technologies: [
+			{
+				name: 'React',
+			},
+			{
+				name: 'React.lazy',
+			},
+			{
+				name: 'Redux',
+			},
+			{
+				name: 'Redux-Thunk',
+			},
+			{
+				name: 'react-relay (by Facebook)',
+			},
+			{
+				name: 'GraphQL',
+			},
+		],
+		pictures: [],
+	},
+	{
+		name: 'Newsroom Control System',
+		status: {
+			cssclass: 'inwork',
+			label: 'coming soon',
+		},
+		demourl: {
+			label: 'coming soon',
+			url: '',
+		},
+		repourl: {
+			label: 'coming soon',
+			url: '',
+		},
+		technologies: [
+			{
+				name: 'React.js',
+			},
+			{
+				name: 'React.lazy',
+			},
+			{
+				name: 'Redux',
+			},
+		],
+		pictures: [
+			{
+				name: 'Rundown Mockup',
+				src: 'pictures/nrcs_preview1.png',
+			},
+			{
+				name: 'Story Editor Mockup',
+				src: 'pictures/nrcs_preview2.png',
+			},
+		],
+	},
+];
