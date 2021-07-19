69781dc470f36acd061c26e63a00bfe88f819fa2
cscheffauer <cscheffauer@rossvideo.com>
2021-07-19 10:17:18 +0200

    updated skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -2,13 +2,51 @@ export const skills = [
     {
         id: 'react',
         text: 'React.js',
-    },
-    {
-        id: 'redux',
-        text: 'Redux',
+        group: '1'
     },
     {
         id: 'typescript',
         text: 'Typescript',
+        group: '1'
+    },
+    {
+        id: 'javascript',
+        text: 'Javascript',
+        group: '1'
+    },
+    {
+        id: 'styledcomp',
+        text: 'Styled-Components',
+        group: '1'
+    },
+    {
+        id: 'redux',
+        text: 'Redux',
+        group: '2'
+    },
+    {
+        id: 'rxjs',
+        text: 'RxJS',
+        group: '2'
+    },
+    {
+        id: 'observables',
+        text: 'Observables',
+        group: '2'
+    },
+    {
+        id: 'jest',
+        text: 'Jest',
+        group: '3'
+    },
+    {
+        id: 'rtl',
+        text: 'React Testing Library',
+        group: '3'
+    },
+    {
+        id: 'playwright',
+        text: 'Playwright',
+        group: '3'
     },
 ]
